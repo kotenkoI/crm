dc2102a8c310d146c9cb210bb19c1fb46055007b
kotenkoI <iryna.herashchenko@gmail.com>
2024-10-18 18:34:06 +0300

    prevented undefined errors

--- src/app/components/promotion-form.tsx
+++ src/app/components/promotion-form.tsx
@@ -53,6 +53,11 @@ export default function PromotionForm({
   });
 
   const handleSubmit = async (values: PromotionFieldValues) => {
+    if (!company) {
+      console.error('Company data is undefined');
+      return;
+    }
+
     await mutateAsync({
       ...values,
       discount: Number(values.discount) || 0,
